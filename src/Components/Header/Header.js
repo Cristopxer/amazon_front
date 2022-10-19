452e39bd48db369f8aa9f47971e91c291a736996
cristopher <cristopherortiz457@gmail.com>
2022-10-18 19:02:09 -0600

    Search bar logo added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div>This is a Header</div>
-  )
+    <div>
+      <nav className="header">
+        <Link to="/">
+          <img
+            src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+            alt="Amazon Logo"
+            className="header_logo"
+          />
+        </Link>
+        <input type="text" className="header_searchInput" />
+      </nav>      
+    </div>
+  );
 }
 
-export default Header
+export default Header;
